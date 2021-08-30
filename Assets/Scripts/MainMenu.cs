6e554552ef1e3e1a31ce2b9c68572d550c03afd6
Stratton Brooks <69229545+szbrooks2017@users.noreply.github.com>
2021-08-30 18:21:33 -0500

    Adding colorblind mechanics

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,8 +7,22 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
 
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     public void PlayMaze()
     {
+        if (colorblindMode.isOn == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene("maze");
     }
 
